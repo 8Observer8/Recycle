17917fc618f37b0f939d77c13f6fa93d1950a4af
Ivan <8observer8@gmail.com>
2013-05-05 09:54:32 +0400

    For storage of incoming information.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Ivan
+ *
+ * Created on May 5, 2013, 9:39 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
